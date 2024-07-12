b40c7eb9d5b30038110e007b439d0c099c4a5a47
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-07-12 16:32:14 +0200

    feat: improve DevTools

--- client/src/components/Game/Hud/DevTools/DevTools.tsx
+++ client/src/components/Game/Hud/DevTools/DevTools.tsx
@@ -1,10 +1,33 @@
-import { MenuItem, Select, Slider } from "@mui/material";
+import {
+  createTheme,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Slider,
+  ThemeProvider,
+} from "@mui/material";
 import { useState, ChangeEvent, useEffect } from "react";
 import { socket } from "../../../../socket";
 import { Section } from "../components/Section";
 import { Button } from "../components/Button";
 import { Time } from "./Time";
 import { Window } from "../components/Window";
+import { useTime } from "./useTime";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      paper: "#006308",
+    },
+    text: {
+      primary: "#fafafa",
+      secondary: "#46505A",
+    },
+    action: {
+      active: "#001E3C",
+    },
+  },
+});
 
 function freezeTime(time: number) {
   socket.emit("freezeTime", time);
@@ -21,7 +44,9 @@ export function DevTools() {
   const [frozen, setFrozen] = useState<boolean>(false);
   const [timeInterval, setTimeInterval] = useState<number>(dayLength);
 
-  function handleSelect(e: ChangeEvent<HTMLSelectElement>) {
+  const formattedTime = useTime(time);
+
+  function handleSelect(e: SelectChangeEvent<number>) {
     setTimeInterval(Number(e.target.value));
   }
 
@@ -50,6 +75,10 @@ export function DevTools() {
     setTime(time);
   }
 
+  function valuetext() {
+    return `${formattedTime}°C`;
+  }
+
   useEffect(() => {
     socket.on("time", fetchTime);
 
@@ -62,32 +91,56 @@ export function DevTools() {
     return null;
   }
 
+  const marks = [
+    {
+      value: Date.now() - timeInterval * 0.75,
+      label: ".75",
+    },
+    {
+      value: Date.now() - timeInterval * 0.5,
+      label: ".50",
+    },
+    {
+      value: Date.now() - timeInterval * 0.25,
+      label: ".25",
+    },
+  ];
+
   return (
     <Window title="Developer Tools">
       <Section title="Time Control">
-        <Time time={time} />
+        <Time timestamp={time} />
         {frozen && timeInterval && (
           <>
-            <Select
-              labelId="time-interval-select"
-              id="time-interval-select"
-              value={timeInterval}
-              label="Age"
-              onChange={() => handleSelect}
-            >
-              <MenuItem value={dayLength}>1 Day</MenuItem>
-              <MenuItem value={dayLength * 3}>3 Day</MenuItem>
-              <MenuItem value={dayLength * 7}>1 Week</MenuItem>
-              <MenuItem value={dayLength * 30}>1 Month</MenuItem>
-              <MenuItem value={dayLength * 30 * 3}>3 Month</MenuItem>
-              <MenuItem value={dayLength * 30 * 6}>6 Month</MenuItem>
-            </Select>
-            <Slider
-              value={time}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => handleSlider(e)}
-              max={Date.now()}
-              min={Date.now() - timeInterval}
-            />
+            <ThemeProvider theme={theme}>
+              <Select
+                sx={{ color: "text.primary" }}
+                labelId="time-interval-select"
+                id="time-interval-select"
+                value={timeInterval}
+                label="Age"
+                onChange={(e: SelectChangeEvent<number>) => handleSelect(e)}
+                color="primary"
+              >
+                <MenuItem value={dayLength}>1 Day</MenuItem>
+                <MenuItem value={dayLength * 3}>3 Day</MenuItem>
+                <MenuItem value={dayLength * 7}>1 Week</MenuItem>
+                <MenuItem value={dayLength * 30}>1 Month</MenuItem>
+                <MenuItem value={dayLength * 30 * 3}>3 Month</MenuItem>
+                <MenuItem value={dayLength * 30 * 6}>6 Month</MenuItem>
+              </Select>
+              <Slider
+                sx={{ color: "text.primary" }}
+                getAriaValueText={valuetext}
+                key={timeInterval}
+                value={time}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => handleSlider(e)}
+                max={Date.now()}
+                min={Date.now() - timeInterval}
+                marks={marks}
+                color="primary"
+              />
+            </ThemeProvider>
           </>
         )}
         {frozen ? (
